5ee21db6406f9b7173247e492a591d3aa1461123
mitsu17 <66157250+mitsu17@users.noreply.github.com>
2020-10-19 02:38:50 +0900

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  # has_one_attached :clip
-  
+  has_one_attached :clip
+  belongs_to :user
   validates :title, presence: true
   validates :text, presence: true
 end
